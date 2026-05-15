327cc9ac105daa7fdb7a61031c21330ec2177988
wa00 (Dmitry Popov) <chronoxnova@yandex.ru>
2026-05-15 15:11:55 +0700

    add Russian

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -7,11 +7,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './en.json';
+import ru from './ru.json';
 
 const RTL_LANGUAGES = ['ar', 'he', 'fa', 'ur'];
 
 i18n.use(initReactI18next).init({
-  resources: { en: { translation: en } },
+  resources: {
+  en: { translation: en },
+  ru: { translation: ru },
+  },
   lng: navigator.language?.split('-')[0] || 'en',
   fallbackLng: 'en',
   interpolation: { escapeValue: false },
